4d27c553702b111980433dead39c0fbacaf7fae6
YuriyV86 <vyuriks@gmail.com>
2023-08-28 23:16:26 +0300

    user crud

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,68 @@
+console.clear()
+
 // Підключаємо технологію express для back-end сервера
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
+
+// ================================================================
+
+class User {
+  static #list = []
+
+  verifyPassowrd = (password) => {
+    this.password === password
+  }
+
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
+
+  static add = (user) => {
+    this.#list.push(user)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+
+  static getById = (id) => {
+    this.#list.find((user) => user.id === id)
+  }
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (user) => user.id === id,
+    )
+
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const user = this.getById(id)
+    if (user) {
+      this.update(user, data)
+
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (user, { email }) => {
+    if (email) {
+      user.email = email
+    }
+  }
+}
 
 // ================================================================
 
@@ -11,12 +72,65 @@ const router = express.Router()
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
+  const list = User.getList()
+
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+
+    data: {
+      users: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
   // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/user-create', function (req, res) {
+  const { email, login, password } = req.body
+
+  const user = new User(email, login, password)
+
+  User.add(user)
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'Користувач створений',
+  })
+})
+
+router.get('/user-delete', function (req, res) {
+  const { id } = req.query
+
+  User.deleteById(Number(id))
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'Користувача видалено',
+  })
+})
+
+router.post('/user-update', function (req, res) {
+  let result = false
+  const { email, password, id } = req.body
+
+  const user = User.getById(Number(id))
+  if (user.verifyPassowrd(password)) {
+    User.update(user, { email })
+    result = true
+  }
+
+  // result = User.updateById(Number(id), { email })
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: result
+      ? 'Користувача модифіковано'
+      : 'Помилка оновлення',
+  })
 })
 
 // ================================================================
